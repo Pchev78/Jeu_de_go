3dfad8955fac5b098e4c15b221b82f2bf1cf8ae5
Pchev78 <119690046+Pchev78@users.noreply.github.com>
2023-12-13 13:57:44 +0100

    Ajout du début des tests couvrant les exceptions

---
+++ tests/GobanTestSprintsKo.java
@@ -0,0 +1,22 @@
+import go.Goban;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GobanTestSprintsKo {
+    private static Goban goban = new Goban();
+
+    @Test
+    void boardsize() {
+        assertEquals(NumberFormatException, goban.boardsize(1));
+        assertEquals(NumberFormatException, goban.boardsize(26));
+        assertEquals(NumberFormatException, goban.boardsize());
+        assertEquals(IllegalArgumentException, goban.boardsize("e"));
+    }
+	
+	@Test
+	void play() {
+		// assertEquals();
+		
+	}
+}
